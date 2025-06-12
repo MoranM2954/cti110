a819c84abcaed660b4423a804971bbcea9c22e71
MoranM2954 <moranm2954@student.faytechcc.edu>
2025-06-12 19:12:58 -0400

    Add files via upload
    Python Project: Welcome to class!

---
+++ P1LAB1_MoranMaria.py
@@ -0,0 +1,9 @@
+#Maria Moran
+#June 12, 2025
+#P1LAB1
+#Project_Welcome to this course!
+
+# Create a program that allows the user to enter their first name and last name. 
+last_name = input("Enter your last name: ")
+first_name = input("Enter your first name: ")
+print("Hello", first_name, last_name, "!", "Welcome to CTI110!")
